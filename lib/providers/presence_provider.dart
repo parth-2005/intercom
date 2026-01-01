adc44e9b787e1555754f74a893b953005b0d846e
parth-2005 <pjpanot260305@gmail.com>
2026-01-02 00:19:41 +0530

    feat: Refactor presence handling with initial stream attachment and subscription management

--- lib/providers/presence_provider.dart
+++ lib/providers/presence_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/user.dart';
 import '../models/signaling_message.dart';
@@ -9,22 +11,39 @@ final presenceProvider = StateNotifierProvider<PresenceNotifier, Map<String, Use
 
 class PresenceNotifier extends StateNotifier<Map<String, User>> {
   PresenceNotifier(this.ref) : super({}) {
+    _attachInitialStream();
     _listenToSignaling();
   }
 
   final Ref ref;
+  StreamSubscription<SignalingMessage>? _subscription;
+
+  void _attachInitialStream() {
+    final current = ref.read(signalingMessagesProvider);
+    current.whenData((stream) {
+      _attachStream(stream);
+    });
+  }
 
   void _listenToSignaling() {
     ref.listen<AsyncValue<Stream<SignalingMessage>?>>(
       signalingMessagesProvider,
       (previous, next) {
         next.whenData((stream) {
-          if (stream != null) {
-            stream.listen(_handleSignalingMessage);
-          }
+          _attachStream(stream);
         });
       },
     );
+  }
+
+  void _attachStream(Stream<SignalingMessage>? stream) {
+    // Avoid duplicate listeners
+    _subscription?.cancel();
+    _subscription = null;
+
+    if (stream == null) return;
+
+    _subscription = stream.listen(_handleSignalingMessage);
   }
 
   void _handleSignalingMessage(SignalingMessage message) {
@@ -44,5 +63,11 @@ class PresenceNotifier extends StateNotifier<Map<String, User>> {
       ...state,
       username: User(username: username, isOnline: isOnline),
     };
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
   }
 }
